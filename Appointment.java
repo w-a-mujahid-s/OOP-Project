92f2130a17e098e0a4f281bc53151b775c3fdf5a
w-a-mujahid-s <wamujahids@gmail.com>
2025-05-02 00:06:00 +0800

    Update Appointment.java

--- Appointment.java
+++ Appointment.java
@@ -1,13 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package AssignmentOOP;
-
-/**
- *
- * @author wamuj
- */
+import java.util.Date;
 public class Appointment {
+    private String apptId;
+    private Date date;
+    private String venue;
+
+    public String getApptId() { 
+        return apptId; 
+    }
     
+    public Date getDate() { 
+        return date; 
+    }
+    
+    public String getVenue() { 
+        return venue; 
+    }
+
+    public void setDate(Date date) { 
+        this.date = date; 
+    }
+
+    public void schedule(String apptId, Date date, String venue) {
+        this.apptId = apptId;
+        this.date = date;
+        this.venue = venue;
+    }
+
+    public void cancelAppointment() {
+        this.apptId = null;
+        this.date = null;
+        this.venue = null;
+        System.out.println("Appointment has been cancelled.");
+    }
 }
